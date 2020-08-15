d4fe94af4a4b1e040d57219d849891706f6822db
vova <vovanychkov95@gmail.com>
2020-08-15 18:27:05 +0300

    first commit

---
+++ app/src/main/java/com/kovrizhkin/kudagofeed_a/presentation/view/screens/details/EventDetailsActivity.kt
@@ -0,0 +1,13 @@
+package com.kovrizhkin.kudagofeed_a.presentation.view.screens.details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kovrizhkin.kudagofeed_a.R
+
+class EventDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_event_details)
+    }
+}
---
+++ app/src/main/java/com/kovrizhkin/kudagofeed_a/presentation/view/screens/feed/FeedActivity.kt
@@ -0,0 +1,13 @@
+package com.kovrizhkin.kudagofeed_a.presentation.view.screens.feed
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kovrizhkin.kudagofeed_a.R
+
+class FeedActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
